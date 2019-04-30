400a248fcabbbea2f0be0cec5d0ea61a797aa04d
Merline Belizaire <merline@noukod.com>
2019-04-30 19:19:55 +0000

    Automatically backed up by Learn

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,22 +1,22 @@
 // Code ClickityClick Component Here
 import React from 'react';
- 
+
 class ClickityClick extends React.Component {
   constructor() {
     super();
- 
+
     // Define the initial state:
     this.state = {
       hasBeenClicked: false
     };
   }
-  
+
   handleClick = () => {
   this.setState({
     hasBeenClicked: true,
   }, () => console.log(this.state.hasBeenClicked));
 }
- 
+
   render() {
     return (
       <div>
